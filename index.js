e892e0939f5351c7001d04f5857e1343cd14c9ec
Michal Swoboda <svobod23@gmail.com>
2022-02-09 15:48:03 +0100

    endpoint to get general word introduced

--- index.js
+++ index.js
@@ -13,7 +13,35 @@ const NOUNS_PER_LENGTH = new Map()
 const VERBS_PER_LENGTH = new Map()
 const OTHERS_PER_LENGTH = new Map()
 
+function getRandomWordWithLengthGeneral(length, response) {
+    let allMaps = [NOUNS_PER_LENGTH, ADJECTIVES_PER_LENGTH, VERBS_PER_LENGTH, OTHERS_PER_LENGTH]
+
+    let filtered = allMaps.filter(coll => coll.has(length));
+
+    if (filtered.length === 0) {
+        response.status(400).send('[HTTP 400] Have no words with length ' + length)
+        return
+    }
+
+    let collection = filtered[Math.floor(Math.random() * (filtered.length - 1))]
+    let wordsByLength = collection.get(length)
+    const responseData = {
+        word: collection.get(length)[Math.floor(Math.random() * (wordsByLength.length - 1))]
+    }
+    let totalCount = 0
+    filtered.forEach(coll => {
+        totalCount += coll.get(length).length
+    })
+    response.setHeader('X-Total-Count', totalCount - filtered.length)
+    response.type('application/json').end(JSON.stringify(responseData))
+}
+
 function getRandomWordWithLength(length, type, response) {
+    if ('general' === type) {
+        getRandomWordWithLengthGeneral(length, response)
+        return
+    }
+
     let collection = obtainCollection(type)
     if (!collection.has(length))
         response.status(400).send('[HTTP 400] Have no word of type ' + type + ' with length ' + length)
@@ -27,18 +55,48 @@ function getRandomWordWithLength(length, type, response) {
 }
 
 function obtainCollection(type) {
-    switch(type) {
-        case 'noun' : return NOUNS_PER_LENGTH
-        case 'verb' : return VERBS_PER_LENGTH
-        case 'adjective' : return ADJECTIVES_PER_LENGTH
-        case 'other' : return OTHERS_PER_LENGTH
-        default : console.log('Have no words with type ' + type)
+    switch (type) {
+        case 'noun' :
+            return NOUNS_PER_LENGTH
+        case 'verb' :
+            return VERBS_PER_LENGTH
+        case 'adjective' :
+            return ADJECTIVES_PER_LENGTH
+        case 'other' :
+            return OTHERS_PER_LENGTH
+        default :
+            console.log('Have no words with type ' + type)
     }
 
     return undefined;
 }
 
+function handleAllCategories(word, response) {
+    let wordLength = word.length.toString()
+    let valid = false
+    let allMaps = [NOUNS_PER_LENGTH, ADJECTIVES_PER_LENGTH, VERBS_PER_LENGTH, OTHERS_PER_LENGTH]
+
+    if (!allMaps.find(coll => coll.has(wordLength))) {
+        response.status(400).send('[HTTP 400] Have no words with length ' + wordLength)
+        return
+    }
+
+    allMaps.find(coll => {
+        if (coll.has(wordLength)) {
+            if (coll.get(wordLength).includes(word))
+                valid = true
+        }
+    })
+
+    response.status(valid ? 200 : 404).send()
+}
+
 function validateWord(word, type, response) {
+    if ('general' === type) {
+        handleAllCategories(word, response)
+        return
+    }
+
     let wordLength = word.length.toString()
 
     let collection = obtainCollection(type)
@@ -89,15 +147,13 @@ app.get('/other/check/:word', function (req, res) {
     validateWord(req.params.word, 'other', res)
 })
 
-//TODO
-// app.get('/word/random/:length', function (req, res) {
-//     getRandomWordWithLength(req.params.length, 'word', res)
-// })
+app.get('/word/random/:length', function (req, res) {
+    getRandomWordWithLength(req.params.length, 'general', res)
+})
 
-//TODO
-// app.get('/word/check/:word', function (req, res) {
-//     validateWord(req.params.word, 'word', res)
-// })
+app.get('/word/check/:word', function (req, res) {
+    validateWord(req.params.word, 'general', res)
+})
 
 function init() {
     const allWordsFileNames = getAllFiles.getAllFilesSync(`resources/`).toArray()
@@ -106,7 +162,7 @@ function init() {
         let type = fn.substring(fn.indexOf('type-') + 5, fn.lastIndexOf('-list'))
         let letters = fn.substring(fn.lastIndexOf('-list') + 6, fn.indexOf('-letters'))
         let fileContent = fs.readFileSync(fn)
-        switch(type) {
+        switch (type) {
             case 'others' : {
                 OTHERS_PER_LENGTH.set(letters, fileContent.toString().split('\n'))
                 break
